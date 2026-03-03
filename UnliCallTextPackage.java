59fdab5e6b4f27a95c8940f3dbf52b76b65c513a
rramayuga <58220050+rramayuga@users.noreply.github.com>
2026-03-03 02:47:07 +0000

    yes

--- UnliCallTextPackage.java
+++ UnliCallTextPackage.java
@@ -8,10 +8,10 @@ public class UnliCallTextPackage implements UnliCallsTextOffer {
         }
 
         if (telcoName.equalsIgnoreCase("Smart")) {
-            return "Not offered";
+            return "Not offered. You will be charged extra per use";
         } 
         else if (telcoName.equalsIgnoreCase("Globe")) {
-            return "Offered for Globe Subscribers";
+            return "Offered for Globe Subscribers. You will be charged extra for other networks calls and texts";
         } 
         else if (telcoName.equalsIgnoreCase("Dito")) {
             return "Offered to all networks";
